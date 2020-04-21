6f88ae21d28a7bd481d956ccab3ca53e25931d60
Mustafa hussien <safi_hp@yahoo.com>
2020-04-21 09:15:36 +0300

    finished

---
+++ Web/Controllers/PortfolioItemsController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using InfraStructure;
+using Web.ViewModels;
+using System.IO;
+using Core.InterFaces;
+
+namespace Web.Controllers
+{
+    public class PortfolioItemsController : Controller
+    {
+        private readonly IUnitOfWork<PortfolioItem> _portfolio;
+
+        //private readonly DataContext _context;
+        private readonly IHostingEnvironment _hosting;
+
+        public PortfolioItemsController(IUnitOfWork<PortfolioItem> portfolio, IHostingEnvironment hosting)
+        {
+            //  _context = context;
+            _portfolio = portfolio;
+            _hosting = hosting;
+        }
+
+        // GET: PortfolioItems
+        public IActionResult Index()
+        {
+            return View(_portfolio.Entity.GetAll());
+        }
+
+        // GET: PortfolioItems/Details/5
+        public IActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var portfolioItem = _portfolio.Entity.GetByID(id);
+
+            if (portfolioItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(portfolioItem);
+        }
+
+        // GET: PortfolioItems/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PortfolioItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PortfolioViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.File != null)
+                {
+                    string upload = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
+                    string fullpath = Path.Combine(upload, model.File.FileName);
+                    model.File.CopyTo(new FileStream(fullpath, FileMode.Create));
+                }
+                PortfolioItem portfolioItem = new PortfolioItem
+                {
+                    ProjectName = model.ProjectName,
+                    Description = model.Description,
+                    ImageUrl = model.File.FileName
+                };
+                _portfolio.Entity.Insert(portfolioItem);
+                _portfolio.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: PortfolioItems/Edit/5
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var portfolioItem = _portfolio.Entity.GetByID(id);
+            if (portfolioItem == null)
+            {
+                return NotFound();
+            }
+            PortfolioViewModel portfolioViewModel = new PortfolioViewModel
+            {
+                Id = portfolioItem.Id,
+                Description = portfolioItem.Description,
+                ImageUrl = portfolioItem.ImageUrl,
+                ProjectName = portfolioItem.ProjectName
+            };
+            return View(portfolioViewModel);
+        }
+
+        // POST: PortfolioItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id,PortfolioViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+           
+            if (ModelState.IsValid)
+            {
+
+                try
+                {
+                    if (model.File != null)
+                    {
+                        string upload = Path.Combine(_hosting.WebRootPath, @"img\portfolio");
+                        string fullpath = Path.Combine(upload, model.File.FileName);
+                        model.File.CopyTo(new FileStream(fullpath, FileMode.Create));
+                    }
+                    if (model.File != null)
+                    {
+                        PortfolioItem portfolioItem = new PortfolioItem
+                        {
+                            Id = model.Id,
+                            ProjectName = model.ProjectName,
+                            Description = model.Description,
+                            //_entity ?? (_entity = new GenericRepository<T>(_context))
+                            ImageUrl = model.File.FileName
+
+                        };
+                        _portfolio.Entity.Update(portfolioItem);
+                    }
+                    else
+                    {
+                        PortfolioItem portfolioItem = new PortfolioItem
+                        {
+                            Id = model.Id,
+                            ProjectName = model.ProjectName,
+                            Description = model.Description,
+
+                            ImageUrl = ""
+
+                        };
+                        _portfolio.Entity.Update(portfolioItem);
+                    }
+
+                   
+                    _portfolio.Save();
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PortfolioItemExists(model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: PortfolioItems/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var portfolioItem = _portfolio.Entity.GetByID(id);
+
+            if (portfolioItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(portfolioItem);
+        }
+
+        // POST: PortfolioItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            _portfolio.Entity.Delete(id);
+
+            _portfolio.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PortfolioItemExists(Guid id)
+        {
+            return _portfolio.Entity.GetAll().Any(e => e.Id == id);
+        }
+    }
+}
